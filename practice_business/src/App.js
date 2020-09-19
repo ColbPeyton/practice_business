cdb2b2542342d56e25914ab9f7350641a4f47509
Colby Peyton <colby.peyton@gmail.com>
2020-09-19 16:40:24 -0500

    created initial 404 page. Updated styling. Working on routes

--- practice_business/src/App.js
+++ practice_business/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
 import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Page404 from './components/Page404';
+
 import './App.css';
 
 
 function App() {
+
+  const routes = [
+    <NavLink activeClassName="selected" to="/">Home</NavLink>,
+    <NavLink activeClassName="selected" to="/about">About</NavLink>,
+    <NavLink activeClassName="selected" to="/services">Services</NavLink>
+  ]
   return (
     <div className="App">
-      <Header />
+      <Header routes={routes}/>
+      <Router >
+              <Switch>
+                <Route component={About} path="/about" />
+                <Route component={Services} path="/services" />
+                <Route exact component={Home} path="/" />
+                <Route component={Page404} />
+              </Switch>
+      </Router>
     </div>
   );
 }
--- practice_business/src/components/Header.js
+++ practice_business/src/components/Header.js
@@ -3,46 +3,63 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    NavLink
   } from "react-router-dom";
 
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 
-function Header(){
-    return(
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/services">Services</Link>
-                    </li>
-                    </ul>
-                </nav>
+import '../styles/Header.scss';
 
-            {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-            <Switch>
-                <Route path="/about">
-                <About />
-                </Route>
-                <Route path="/services">
-                <Services />
-                </Route>
-                <Route path="/">
-                <Home />
-                </Route>
-            </Switch>
+function Header(props){
+    console.log(props.routes)
+    return(
+        <header className='header-container'>
+            <div className='header-contents'>
+                <div className='top-bar'>
+                    <div className='top-info'>
+                        <button>(555)555-5555</button>
+                        <button>Request An Appointment</button>
+                        <button>Our Location</button>
+                    </div>
+                </div>
+                <div className='bottom-bar'>
+                    <div className='bottom-logo'>
+                        <button>Logo</button>
+                    </div>
+                    <Router>
+                        <div className='bottom-nav'>
+                            <nav>
+                                <ul>
+                                <li>
+                                    {props.routes[0]}
+                                </li>
+                                <li>
+                                    {props.routes[1]}
+                                </li>
+                                <li>
+                                    {props.routes[2]}
+                                </li>
+                                </ul>
+                            </nav>
+                        {/* <Switch>
+                            <Route path="/about">
+                            <About />
+                            </Route>
+                            <Route path="/services">
+                            <Services />
+                            </Route>
+                            <Route path="/">
+                            <Home />
+                            </Route>
+                        </Switch> */}
+                    </div>
+                </Router>
+                </div>
             </div>
-    </Router>
+    </header>
     )
 }
 
