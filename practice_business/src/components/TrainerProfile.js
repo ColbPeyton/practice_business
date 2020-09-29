d7d798f1eed186aad2444586fdf32cc5c086313d
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-09-29 17:16:05 -0500

    Stlying almost completed for devices smaller than 789px.
    Updated styling for smaller screens. Does not include landscape on mobile devices. Will be completed at a later time

--- practice_business/src/components/TrainerProfile.js
+++ practice_business/src/components/TrainerProfile.js
@@ -5,13 +5,15 @@ import '../styles/TrainerProfile.scss';
 function TrainerProfile(props){
     return(
         <div className={`profile ${props.id % 2 === 0 ? 'even' : ''}`}>
-            <div className='profile-img'>
-                <img src={props.image.image} alt={props.image.alt} />
-            </div>
-            <div className='profile-info'>
-                <p className='name'>{props.image.name}</p>
-                <p className='focus'>{props.image.focus}</p>
-                <p className='desc'>{props.image.desc}</p>
+            <div className='profile-container'>
+                <div className='profile-img'>
+                    <img src={props.image.image} alt={props.image.alt} />
+                </div>
+                <div className='profile-info'>
+                    <p className='name'>{props.image.name}</p>
+                    <p className='focus'>{props.image.focus}</p>
+                    <p className='desc'>{props.image.desc}</p>
+                </div>
             </div>
         </div>
     );
