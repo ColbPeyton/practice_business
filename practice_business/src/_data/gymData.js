872c824dc80389d67d3ff6ad711ae3836d187e15
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-09-30 19:24:52 -0500

    Added thumbnail images and started styling Services component.
    Thumbnail images are used for for Simple React Lightbox. Styling component for 320px. Once completed, will style for others.

--- practice_business/src/_data/gymData.js
+++ practice_business/src/_data/gymData.js
@@ -1,22 +1,27 @@
 import importImages from '../_helpers/importImages';
 
 const images = importImages(require.context('../images/gym', false, /\.(png|jpe?g|svg)$/));
+const imagesThumb = importImages(require.context('../images/gym/thumbnails', false, /\.(png|jpe?g|svg)$/));
 
 const gymImages = [
     {
         img: images[0],
+        thumb: imagesThumb[0],
         alt: 'Weight Room'
     },
     {
         img: images[1],
+        thumb: imagesThumb[1],
         alt: 'Privacy Gym'
     },
     {
         img: images[2],
+        thumb: imagesThumb[2],
         alt: 'Weight Room Purple Wall'
     },
     {
         img: images[3],
+        thumb: imagesThumb[3],
         alt: 'Weight Room Open Floor'
     },
 ];
--- practice_business/src/components/Services.js
+++ practice_business/src/components/Services.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { SRLWrapper } from "simple-react-lightbox";
+import {NavLink} from "react-router-dom";
+
 
 import gymImages from '../_data/gymData';
 
@@ -11,7 +13,7 @@ function Services(){
         return gymImages.map((image, index) => {
             return (
                 <a href={image.img}  key={index} data-attribute="SRL">
-                    <img src={image.img} alt={image.alt} />
+                    <img src={image.thumb} alt={image.alt} />
                 </a>
             )
         })
@@ -26,10 +28,44 @@ function Services(){
                     <h2>We Got The Goods</h2>
                 </div>
                 <div className='body-container'>
-                <SRLWrapper>
+                <div className='body-about'>
+                        <h3>Let's Lift</h3>
+                        <p>We have the equipment, space, and trainers to create the motivation to get you into the best shape of your life.</p>
+                    </div>
+                    <div className='body-information'>
+                        <div className='info'>
+                            <h3>2000ft Weight Room</h3>
+                            <p>Plenty of equipment for the most busy of days.</p>
+                        </div>
+                        <div className='info'>
+                            <h3>Full Boxing Ring</h3>
+                            <p>Practice working the ring where champions are made.</p>
+                        </div>
+                        <div className='info'>
+                            <h3>Complete Crossfit Area</h3>
+                            <p>Jump, lift, and slam to your hearts content.</p>
+                        </div>
+                        <div className='info'>
+                            <h3>Private Gym Rooms</h3>
+                            <p>We understand that working out is a personal thing. Take your time in the private Weight room</p>
+                        </div>
+                    </div>
+                <SRLWrapper className='body-images'>
                     {assignImages()}
                 </SRLWrapper>
+
                 </div>
+                    <div className='footer-container'>
+                        <div className='footer-tagline'>
+                            <h3>Let's Get You In Here</h3>
+                        </div>
+                        <div className='button-container'>
+                            <NavLink className='link' to='/appointment'>
+                                <p>Let's Get To Work</p>
+                            </NavLink>
+                        </div>
+                    </div>
+                    
             </div>
         </main>
     );
