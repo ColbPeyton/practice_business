741be3d1c5569ac768ab72364b51f5289d506d49
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-10-21 17:33:48 -0500

    Fixing error with added logic in production

--- practice_business/src/components/Sidebar.js
+++ practice_business/src/components/Sidebar.js
@@ -25,7 +25,7 @@ function Sidebar(props){
 
     // Detect if mobile device, set to pageYOffset, set 0
     useEffect(()=>{
-        if(isMobileDevice()){
+        if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)){
             setHeight(window.pageYOffset);
         }else{
             setHeight(0);
@@ -41,16 +41,16 @@ function Sidebar(props){
     // Update position of sidebar based on scroll
     function handleScroll(){
         // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        if(isMobileDevice()){
+        if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)){
             setHeight(window.pageYOffset);
         }else{
             setHeight(0);
         }
     }
 
-    function isMobileDevice() {
-        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    };
+    // function isMobileDevice() {
+    //     return ();
+    // };
 
     return(
         <div className={`sidebar`} style={ {animation: `${active ? "slideIn" : "slideOut"} 0.5s forwards`} }>
